ca358f652217bcf7eb5f64a03735f556ee68c375
Symbiont23707 <71516569+Symbiont23707@users.noreply.github.com>
2021-09-15 21:37:26 +0300

    added first task

---
+++ EvgenyBykov/Task1.py
@@ -0,0 +1,9 @@
+print("Task 1.1:") #для удобства в проверке
+
+str1 = "Hello"
+def calculate_without_len(str1):
+    length_str = 0
+    for i in str1:
+        length_str += 1
+    return length_str
+print(calculate_without_len(str1)) #для удобства в проверке
