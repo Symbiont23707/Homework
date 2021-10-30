40f480a2012e8c30d8c52ab8ae23b3af9db9389c
Symbiont23707 <71516569+Symbiont23707@users.noreply.github.com>
2021-10-30 19:02:50 +0300

    Iteration 2
    use PYInstaller,setuptools, running the //python rss_reader.py application on //rss_reader

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+Version = "2.0"
+Description = "Parser who parse rss sites"
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements = f.read()
+
+setup(
+    name="rss-reader",
+    author="Bykov Evgeny",
+    version=Version,
+    description=Description,
+    author_email="146iqmega1hp@gmail.com",
+    packages=find_packages("rss_packaging"),
+    include_package_data=True,
+    python_requires=">=3.9",
+    install_requires=[],
+    keywords=["python"],
+    entry_points={
+        "console_scripts": [
+            "rss_reader = rss_reader:main",
+        ]
+    },
+)
+
+
