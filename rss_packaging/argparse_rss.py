d9dd5cfb0279cbd9725fed277c872ac0591de9c0
Symbiont23707 <71516569+Symbiont23707@users.noreply.github.com>
2021-10-30 18:46:22 +0300

    Iteration 2
    use PYInstaller, running the //python rss_reader.py application on //rss_reader

--- rss_packaging/argparse_rss.py
+++ rss_packaging/argparse_rss.py
@@ -6,7 +6,7 @@ class Argsparse_rss:
         # argparser with some functions, such as --version, --json, --verbose, --limit
         parser = argparse.ArgumentParser(description="Pure Python command-line RSS reader.")
 
-        parser.add_argument("--version", help="Print version info", action="version", version='"Version 1.0"')
+        parser.add_argument("--version", help="Print version info", action="version", version='"Version 2.0"')
         parser.add_argument("--json", help="Print result as JSON in stdout", action="store_true")
         parser.add_argument("--verbose", help="Outputs verbose status messages", action="store_true")
         parser.add_argument("--limit", action='store', type=int, help="Limit news topics if this parameter provided",
